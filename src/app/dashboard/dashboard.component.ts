6570fe75adbfa2fd8deaa69b5933f7dfb2ad77c2
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-08-08 17:12:13 +0200

    stylings

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  currentUser?: any;
+  subscription: Subscription = new Subscription();
 
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+
+      this.subscription.add(
+        this.authService.user$.subscribe(
+          user => this.currentUser = user
+        )
+      );
+
+      console.log(this.currentUser);
+  }
 }
